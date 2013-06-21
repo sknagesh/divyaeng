31324df89f4bb793f5ae1a94af02871e68a4abbe
sknagesh <sknagesh@gmail.com>
2013-06-21 19:18:00 +0530

    view part

--- show_tool_list_and_details_for_op.php
+++ show_tool_list_and_details_for_op.php
@@ -32,19 +32,30 @@ print("<td><a href=\"$ppath\" target=\"_NEW\">NC Program</a></td></tr>");
 
 
 print("</table>");
-$query="SELECT Ope_Tool_ID,t.Tool_Part_NO,t.Tool_Desc,Insert_ID_1,Insert_ID_2,tb1.Brand_Description as bd1,tb2.Brand_Description as bd2,
+/*$query="SELECT Ope_Tool_ID,t.Tool_Part_NO,t.Tool_Desc,Insert_ID_1,Insert_ID_2,tb1.Brand_Description as bd1,tb2.Brand_Description as bd2,
 		tt.Tool_Part_NO as tpn2,tt.Tool_Desc as tde2,ins2.Insert_Part_NO as ip2,ins2.Insert_Description as id2,
 		inse.Insert_Description,
 		t.Tool_Dia,Tool_ID_1,Tool_ID_2,Ope_Tool_OH,Ope_Tool_Desc,Holder_Description FROM Ope_Tool AS ot 
 		INNER JOIN Tool as t ON t.Tool_ID=ot.Tool_ID_1 
 		LEFT OUTER JOIN Inserts AS inse ON inse.Insert_ID=ot.Insert_ID_1
 		LEFT OUTER JOIN Inserts AS ins2 ON ins2.Insert_ID=ot.Insert_ID_2
-		LEFT OUTER JOIN Tool as tt ON tt.Tool_ID=ot.Tool_ID_2 
+		INNER JOIN Tool as tt ON tt.Tool_ID=ot.Tool_ID_2 
 		INNER JOIN Tool_Brand as tb1 ON tb1.Brand_ID=t.Brand_ID
 		INNER JOIN Tool_Brand as tb2 ON tb2.Brand_ID=tt.Brand_ID
 		INNER JOIN Holder AS h ON h.Holder_ID=ot.Holder_ID_1
 		WHERE ot.Operation_ID='$opid';";
+*/
 
+
+
+$query='SELECT Ope_Tool_ID,Ope_Tool_OH,Ope_Tool_Desc,Holder_Description,Tool_ID_1, 
+		(SELECT CONCAT(Tool_Desc," ( ",Tool_Part_NO," )") FROM Tool WHERE Tool_ID=ot.Tool_ID_1) as td1, 
+		(SELECT CONCAT(Tool_Desc," ( ",Tool_Part_NO," )") FROM Tool WHERE Tool_ID=ot.Tool_ID_2)as td2, 
+		(SELECT CONCAT(Insert_Part_NO," ",Insert_Description) FROM Inserts WHERE Insert_ID=ot.Insert_ID_1) as i1,
+		(SELECT CONCAT(Insert_Part_NO," ",Insert_Description) FROM Inserts WHERE Insert_ID=ot.Insert_ID_2) as i2,
+		(SELECT Brand_Description FROM Tool_Brand as tb INNER JOIN Tool as t1 ON t1.Brand_ID=tb.Brand_ID WHERE t1.Tool_ID=ot.Tool_ID_1) as tb1,
+		(SELECT Brand_Description FROM Tool_Brand as tb2 INNER JOIN Tool as t2 ON t2.Brand_ID=tb2.Brand_ID WHERE t2.Tool_ID=ot.Tool_ID_2) as tb2 
+		FROM Ope_Tool AS ot INNER JOIN Holder AS h ON h.Holder_ID=ot.Holder_ID_1 WHERE ot.Operation_ID="'.$opid.'";';
 
 
 //print($query);
@@ -54,14 +65,14 @@ $r=mysql_num_rows($res);
 if($r!=0)
 {
 print("<table border=\"1\" cellspacing=\"1\" id=\"ttble\">");
-print("<tr><th>Tool Info</th><th>Preferred Tool/Alternate Tool</th><th>Description</th><th>Insert</th><th>Holder</th><th>Tool Overhang</th></tr>");
+print("<tr><th>Tool ID</th><th>Preferred Tool and Insert</th><th>Alternate Tool and Insert</th><th>Description</th><th>Holder</th><th>Tool Overhang</th></tr>");
 while($row=mysql_fetch_assoc($res))
 {
-	$td=$row['bd1'].' Make '.$row['Tool_Desc']." (".$row['Tool_Part_NO'].") ".$row['Insert_Part_NO'].")";
-	if($row['tpn2']!=""){$td.="<font color=\"red\"> OR </font><font color=\"green\">".$row['bd2'].' Make '.$row['tde2']." (".$row['tpn2'].") ".$row['ip2']." </font>";}
-	print("<tr><td><input type=\"radio\" name=\"tinfo\" class=\"tinfo\" id=\"tinfo\" value=\"$row[Tool_ID_1]\"></input>
-	</td><td>$td</td><td>$row[Ope_Tool_Desc]</td>
-	<td>$row[Insert_Description] <font color=\"green\"> $row[id2]</font></td>
+	if($row['td2']!=''){$t2='<font color=\"green\">'.$row['tb2'].' Make '.$row['td2'].' '.$row['i2'].'</font>';}else{$t2='';}
+	print("<tr><td><input type=\"radio\" name=\"tinfo\" class=\"tinfo\" id=\"tinfo\" value=\"$row[Tool_ID_1]\"></input></td>
+	<td width=\"450px\">$row[tb1] Make $row[td1] $row[i1]</td>
+	<td>$t2</td>
+	<td>$row[Ope_Tool_Desc]</td>
 	<td>$row[Holder_Description]</td><td>$row[Ope_Tool_OH]</td></tr>");
 	
 }
